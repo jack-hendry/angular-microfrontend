5908ea5efd368af8b8fd2c20c55e43495552acef
jack-hendry <jack.r.hendry.p@gmail.com>
2025-07-09 10:24:49 -0400

    Migrating first app

--- projects/login/src/app/app-routing.module.ts
+++ projects/login/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+// app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./feature/login/login.module').then((m) => m.LoginModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
